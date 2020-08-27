572f87e1bf62195d2adaa4ecc19d15b0cacd82c3
root <root@9d1be8b074da>
2020-08-27 23:57:46 +0000

    robot color chooser

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,14 +2,27 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-	    //2. Make the robot draw a shape (this will take more than one line of code)		
+		Robot nugget = new Robot();
+		nugget.setSpeed(100);
+	    //2. Make the robot draw a shape (this will take more than one line of code)
+		nugget.penDown();
+		for (int i = 0; i < 4; i++) {
+			nugget.move(250);
+			nugget.turn(90);
+		}
 		//3. Set the pen width to 10
+		nugget.setPenWidth(10);
+	
 		//4. Ask the user what color pen they would like the robot to draw with
 		//5. Use an if/else statement to set the pen color that the user requested
         //6. If the user doesn't enter anything, choose a random color
