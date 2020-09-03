de3cbce8b948452db0bb3f8b603075bf1ccb4022
root <root@ee82dca2d18c>
2020-09-03 23:55:39 +0000

    Crazy cat lady

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -13,19 +13,44 @@ public class RobotColorChooser {
 		
 		//1. Create a new Robot
 		Robot nugget = new Robot();
+		for (int i = 0; i < 2; ) {
+		nugget.setPenWidth(10);
 		nugget.setSpeed(100);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
 		nugget.penDown();
-		for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			
+		
+			
+		
 			nugget.move(250);
 			nugget.turn(90);
 		}
 		//3. Set the pen width to 10
-		nugget.setPenWidth(10);
+		
 	
 		//4. Ask the user what color pen they would like the robot to draw with
+		
+			
+		
+		String color = JOptionPane.showInputDialog("What color do you want to use (red, green or blue)");
 		//5. Use an if/else statement to set the pen color that the user requested
+		if(color.equalsIgnoreCase("BLUE")) {
+			nugget.setPenColor(Color.BLUE);	
+		}
+		else if(color.equalsIgnoreCase("GREEN")) {
+			nugget.setPenColor(Color.GREEN);
+		}
+		else if (color.equalsIgnoreCase("RED")) {
+			nugget.setPenColor(Color.RED);
+		}
+		else {
+			nugget.setRandomPenColor() ;
+			}
+			
+		}
         //6. If the user doesn't enter anything, choose a random color
+		
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 
