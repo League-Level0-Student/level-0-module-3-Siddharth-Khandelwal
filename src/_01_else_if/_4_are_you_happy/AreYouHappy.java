e220dcce485bb59a25c86d4b462d1e9f49daecf4
root <root@7d7c59b70a08>
2020-09-10 23:46:41 +0000

    Own Adventure

--- src/_01_else_if/_4_are_you_happy/AreYouHappy.java
+++ src/_01_else_if/_4_are_you_happy/AreYouHappy.java
@@ -3,53 +3,28 @@ package _01_else_if._4_are_you_happy;
 import javax.swing.JOptionPane;
 
 public class AreYouHappy {
-public static void main(String[] args) {
-String Mood = JOptionPane.showInputDialog("Are you happy right now ")	;
-if (Mood.equalsIgnoreCase("Yes")) {
-	JOptionPane.showMessageDialog(null, "Keep doing what you are doing");
-}
-else {
-JOptionPane.showInputDialog("Do you want to be happy?")	;
-}
+	public static void main(String[] args) {
+		String Mood = JOptionPane.showInputDialog("Are you happy right now ");
+		if (Mood.equalsIgnoreCase("Yes")) {
+			JOptionPane.showMessageDialog(null, "Keep doing what you are doing");
+		} else {
+			String Hi = JOptionPane.showInputDialog("Do you want to be happy?");
 
+			if (Hi.equalsIgnoreCase("Yes")) {
+				JOptionPane.showMessageDialog(null, "Change Something");
+			}
+			else if(Hi.equalsIgnoreCase("No")) {
+				JOptionPane.showMessageDialog(null, "Keep doing what you are doing");
+			}
+		
+		
+		
+		
+		
+		
+		
+		}
 
+	}
 
-
-
-
-
-
-
-
-
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}  
---
+++ src/_01_else_if/_5_own_adventure/OwnAdventure.java
@@ -0,0 +1,38 @@
+package _01_else_if._5_own_adventure;
+
+import javax.swing.JOptionPane;
+
+public class OwnAdventure {
+public static void main(String[] args) {
+
+	
+	String Hi = JOptionPane.showInputDialog("Your about to go to a show, witch one do you want to go to, a magic show or a talent show? ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
