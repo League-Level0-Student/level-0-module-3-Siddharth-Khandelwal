24e6ce0d70d95148fc1292cc5eed7a62196bfb36
root <root@033c33cc5d34>
2020-09-22 23:55:26 +0000

    Walk of fame

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -5,19 +5,42 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
-		Robot rob = new Robot();
+		Robot nugget = new Robot();
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
+nugget.setX(200);
+nugget.setY(100);
+nugget.setSpeed(100);
 
 		// 2. Make the robot draw a star shape. Hint: angle=144.
+nugget.penDown();
+nugget.setRandomPenColor();
+nugget.setPenWidth(100);
+for (int n = 0; n < 10; n++) {
+	
+	nugget.setY(100);
+	nugget.setAngle(90);
+for (int i = 0; i < 5; i++) {
+	nugget.setRandomPenColor();
 
+	
+nugget.turn(144);
+nugget.move(150);
+}
+
+nugget.penUp();
+
+nugget.move(50);
+nugget.penDown();
+}
+nugget.hide();
 		// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/RobotWalk
+		 // http//:bit.ly/RobotWalk
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
