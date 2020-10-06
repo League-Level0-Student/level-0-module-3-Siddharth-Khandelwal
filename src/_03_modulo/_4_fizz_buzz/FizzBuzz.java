a31f1e14907002b64c1bdf7485a110cd1c02a96e
root <root@f1825c665844>
2020-10-06 23:46:33 +0000

    FizzBuzz

--- src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
+++ src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
@@ -1,6 +1,7 @@
 
 package _03_modulo._4_fizz_buzz;
 
+import javax.swing.JOptionPane;
 
 /**
 * Fizz Buzz
@@ -18,9 +19,35 @@ package _03_modulo._4_fizz_buzz;
 *  
 * 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz
 * 
+* if the number is divisible by 3 and 5, print fizzbuzz
+* else if the number is divisible by 3, print fizz
+* else if the number is divisible by 5, print buzz
+* else print the number.
+* 
+* 
+* 
 **/
 
 public class FizzBuzz {
+public static void main(String[] args) {
+for (int i = 1; i < 21; i++) {
+	if (i%3 == 0&&i%5==0) {
+		System.out.println("fizzbuzz");
+	}else if(i%3==0) {
+		System.out.println("fizz");
+	}else if(i%5==0) {
+		System.out.println("buzz");
+	}else {
+	System.out.println(i);
+	}
+}
 
+	
+	
+	
+	
+	
+	
+}
 }
 
